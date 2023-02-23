4e8b9ecb4562e2d3713a9708c7bd3152a5d0e929
Slobodianyk Maksym <maksym.slobodianyk@emplifi.io>
2023-02-23 11:28:15 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Table from './components/Table';
+
+function App() {
+  return (
+    <div className="App">
+      <Table x={100} y={100}/>
+    </div>
+  );
+}
+
+export default App;
